d936f832fc2e20a34a26cb07eac67586fb8728b5
trylogout <kurtswrc@gmail.com>
2020-08-11 14:38:13 +0500

    v0.0.1
    App updated

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,21 @@ export default {
     return {
       send_to_terminal: "",
       banner: {
-        header: "Sand Shell",
+        header: "E-Corp Shell",
         helpHeader: 'Enter "help" for more information.',
         emoji: {
-          first: "zero",
-          second: " one",
-          time: 750
+          first: "base"
         },
-        sign: `Henry@Ecorp:~ $`
+        sign: `Henry@Ecorp:~#`,
+        img: {
+          align: "left",
+          link:
+            "https://www.e-corp-usa.com/images/home-v2/ecorp_logo_white.png",
+          width: 60,
+          height: 80
+        },
+        dir_parent: " ",
+        dir: "~"
       },
       commands: [
         {
@@ -37,36 +44,21 @@ export default {
           get() {
             return navigator.appVersion;
           }
-        },
-        {
-          name: "ls",
-          get() {
-            return ` 
-   <table class="table">
-   <tbody>
-    <tr>
-      <td bgcolor="red"><p color="blue">efhuiu3rh37d.dat</p></td>
-      <td><p style="color:red;">Applications</p></td>
-      <td>Library</td>
-    </tr>
-    <tr>
-      <td>printers.xml</td>
-      <td>app.2ab771f81c1da05a7c9b.xml</td>
-      <td>id_rsa.pub</td>
-    </tr>
-   </tbody>
-   </table>`;
-          }
         }
       ]
     };
   },
   methods: {
     prompt(value) {
-      if (value.trim().slice(0, 4) === "sudo") {
+      if (
+        value
+          .trim()
+          .slice(0, 4)
+          .toLowerCase() === "sudo"
+      ) {
         this.send_to_terminal = `<p style="color:red;">[ERROR] You are not in the sudoers file. This incident will be reported</p>`;
       } else {
-        if (value.trim() === "ifconfig") {
+        if (value.trim().toLowerCase() === "ifconfig") {
           this.send_to_terminal = `
     Wi-Fi wireless network card:
         
@@ -74,10 +66,28 @@ export default {
     IPv4 address. . . . . . . . .: 192.168.1.2
     Subnet mask. . . . . . . . . : 255.255.255.0
     Default Gateway. . . . . . . : 192.168.1.1`;
-        } else if (value.trim() === "cd Data") {
-          this.$data.banner.sign = "Henry@Ecorp:Data $";
-        } else if (value.trim() === "cd") {
-          this.$data.banner.sign = "Henry@Ecorp:~ $";
+        } else if (value.trim().toLowerCase() === "cd data") {
+          this.$data.banner.sign = "Henry@Ecorp:/Data#";
+        } else if (value.trim().toLowerCase() === "cd") {
+          this.$data.banner.sign = "Henry@Ecorp:~#";
+        } else if (value.trim().toLowerCase() === "ls") {
+          if (this.$data.banner.sign.slice(-5).toLowerCase() === "data#") {
+            this.send_to_terminal = "Data_Files.dat";
+          } else {
+            this.send_to_terminal = `<table class="table" style="width:70%; margin-top: 0px;">
+              <tr>
+                <td><p style="color:red;">efhuiu3rh37d.dat</p></td>
+                <td><p style="color:green;">Applications</p></td>
+                <td><p style="color:green;">Library</p></td>
+              </tr>
+              <tr>
+                <td>printers.xml</td>
+                <td><p style="color:red;">Data</p></td>
+                <td><p>d_rsa.pub</p></td>
+              </tr>
+            </table>`;
+          }
+          this.$data.banner.sign = "Henry@Ecorp:~#";
         } else {
           this.send_to_terminal = `'${value}' is not recognized as an internal command or external,
 an executable program or a batch file`;
