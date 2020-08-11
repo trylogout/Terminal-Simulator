0a19636b18f3dfbb214f582641a6a136847151d6
trylogout <kurtswrc@gmail.com>
2020-08-11 21:37:49 +0500

    v0.0.1
    Some improvements

--- src/App.vue
+++ src/App.vue
@@ -29,20 +29,50 @@ export default {
           width: 60,
           height: 80
         },
+        pwd: "",
         dir_parent: " ",
-        dir: "~"
+        dir: "/home/henry"
       },
       commands: [
-        {
-          name: "info",
-          get() {
-            return `<p>INFO</p>`;
-          }
-        },
         {
           name: "uname",
           get() {
             return navigator.appVersion;
+          }
+        },
+        {
+          name: "sudo",
+          get() {
+            return `<p style="color:red;">[ERROR] You are not in the sudoers file. This incident will be reported</p>`;
+          }
+        },
+        {
+          name: "ifconfig",
+          get() {
+            return `  enp2s0: flags=4163&lt;UP,BROADCAST,RUNNING,MULTICAST&gt;  mtu 1500
+          inet 192.168.1.106  netmask 255.255.255.0  broadcast 192.168.0.255
+          inet6 fe80::8e29:fe4b:875d:213d  prefixlen 64  scopeid 0x20&lt;link&gt;
+          ether 20:89:84:90:2b:cd  txqueuelen 1000  (Ethernet)
+          RX packets 8350  bytes 11271255 (11.2 MB)
+          RX errors 0  dropped 0  overruns 0  frame 0
+          TX packets 5336  bytes 474287 (474.2 KB)
+          TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+  lo: flags=73&lt;UP,LOOPBACK,RUNNING&gt;  mtu 65536
+          inet 127.0.0.1  netmask 255.0.0.0
+          inet6 ::1  prefixlen 128  scopeid 0x10&lt;host&gt;
+          loop  txqueuelen 1000  (Локальная петля (Loopback))
+          RX packets 518  bytes 40060 (40.0 KB)
+          RX errors 0  dropped 0  overruns 0  frame 0
+          TX packets 518  bytes 40060 (40.0 KB)
+          TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0
+
+  wlp3s0: flags=4099&lt;UP,BROADCAST,MULTICAST&gt;  mtu 1500
+          ether 60:6c:66:b3:90:8e  txqueuelen 1000  (Ethernet)
+          RX packets 0  bytes 0 (0.0 B)
+          RX errors 0  dropped 0  overruns 0  frame 0
+          TX packets 0  bytes 0 (0.0 B)
+          TX errors 0  dropped 0 overruns 0  carrier 0  collisions 0`;
           }
         }
       ]
@@ -50,48 +80,78 @@ export default {
   },
   methods: {
     prompt(value) {
-      if (
-        value
-          .trim()
-          .slice(0, 4)
-          .toLowerCase() === "sudo"
-      ) {
-        this.send_to_terminal = `<p style="color:red;">[ERROR] You are not in the sudoers file. This incident will be reported</p>`;
-      } else {
-        if (value.trim().toLowerCase() === "ifconfig") {
-          this.send_to_terminal = `
-    Wi-Fi wireless network card:
-        
-    Local link IPv6 address. . . : fe80 :: 340f: 6f02: 41e9: 477b% 24
-    IPv4 address. . . . . . . . .: 192.168.1.2
-    Subnet mask. . . . . . . . . : 255.255.255.0
-    Default Gateway. . . . . . . : 192.168.1.1`;
-        } else if (value.trim().toLowerCase() === "cd data") {
-          this.$data.banner.sign = "Henry@Ecorp:/Data#";
-        } else if (value.trim().toLowerCase() === "cd") {
-          this.$data.banner.sign = "Henry@Ecorp:~#";
-        } else if (value.trim().toLowerCase() === "ls") {
-          if (this.$data.banner.sign.slice(-5).toLowerCase() === "data#") {
-            this.send_to_terminal = "Data_Files.dat";
-          } else {
-            this.send_to_terminal = `<table class="table" style="width:70%; margin-top: 0px;">
+      if (value.trim().toLowerCase() === "test_temp_dir") {
+        this.send_to_terminal = this.$data.dir
+          .split("\\")
+          .pop()
+          .split("/")
+          .pop();
+      } else if (value.trim().toLowerCase() === "test_par_dir") {
+        this.send_to_terminal = this.$data.dir.slice(
+          0,
+          this.$data.dir.lastIndexOf("/")
+        );
+      } else if (value.trim().toLowerCase() === "cd data") {
+        this.$data.banner.sign = "Henry@Ecorp:/~Data#";
+      } else if (value.trim().toLowerCase() === "cd") {
+        this.$data.banner.sign = "Henry@Ecorp:~#";
+        this.$data.dir = "/home/henry";
+      } else if (value.trim().toLowerCase() === "ls") {
+        if (this.$data.banner.sign.slice(-5).toLowerCase() === "data#") {
+          this.$data.dir = "/home/henry/Data/";
+          this.send_to_terminal = "Data_Files.dat";
+        } else {
+          this.send_to_terminal = `<table class="table" style="width:70%; margin-top: 5px;">
               <tr>
-                <td><p style="color:red;">efhuiu3rh37d.dat</p></td>
-                <td><p style="color:green;">Applications</p></td>
-                <td><p style="color:green;">Library</p></td>
+                <td><p>efhuiu3rh37d.dat</p></td>
+                <td><p style="color:#838383;">Applications</p></td>
+                <td><p style="color:#838383;">Library</p></td>
               </tr>
               <tr>
                 <td>printers.xml</td>
-                <td><p style="color:red;">Data</p></td>
+                <td><p style="color:#838383;">Data</p></td>
                 <td><p>d_rsa.pub</p></td>
               </tr>
             </table>`;
-          }
-          this.$data.banner.sign = "Henry@Ecorp:~#";
-        } else {
-          this.send_to_terminal = `'${value}' is not recognized as an internal command or external,
-an executable program or a batch file`;
         }
+      } else if (value.trim().toLowerCase() === "pwd") {
+        this.send_to_terminal = "PWD_EX";
+      } else if (value.trim().toLowerCase() === "main_test") {
+        this.send_to_terminal = `<table class="table" style="width:70%; margin-top: 5px;">
+    <tr>
+        <td><p style="color:green;">bin</p></td>
+        <td><p style="color:#838383;">dev</p></td>
+        <td><p style="color:green;">lib</p></td>
+        <td><p style="color:green;">libx32</p></td>
+        <td><p style="color:#838383;">mnt</p></td>
+        <td><p style="color:#838383;">root</p></td>
+        <td><p style="color:#838383;">snap</p></td>
+        <td bgcolor=green><p style="color:#151515;">tmp</p></td>
+    </tr>
+    <tr>
+        <td><p style="color:#838383;">boot</p></td>
+        <td><p style="color:#838383;">etc</p></td>
+        <td><p style="color:green;">lib32</p></td>
+        <td><p style="color:#838383;">lost+found</p></td>
+        <td><p style="color:#838383;">opt</p></td>
+        <td><p style="color:#838383;">run</p></td>
+        <td><p style="color:#838383;">srv</p></td>
+        <td><p style="color:#838383;">usr</p></td>
+    </tr>
+    <tr>
+        <td><p style="color:#838383;">cdrom</p></td>
+        <td><p style="color:#838383;">home</p></td>
+        <td><p style="color:green;">lib64</p></td>
+        <td><p style="color:#838383;">media</p></td>
+        <td><p style="color:#838383;">proc</p></td>
+        <td><p style="color:green;">sbin</p></td>
+        <td><p style="color:#838383;">sys</p></td>
+        <td><p style="color:#838383;">var</p></td>
+    </tr>
+</table>`;
+      } else {
+        this.send_to_terminal = `'${value}' is not recognized as an internal command or external,
+an executable program or a batch file`;
       }
     }
   }
